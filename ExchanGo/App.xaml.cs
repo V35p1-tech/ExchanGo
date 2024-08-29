2a99cd83f0246ae8301dc3154bbb0bb6c2000558
V35p1-tech <136362288+V35p1-tech@users.noreply.github.com>
2024-08-29 21:44:43 +0200

    Add project files.

---
+++ ExchanGo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ExchanGo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
